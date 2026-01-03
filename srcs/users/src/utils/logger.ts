f3d74cbbb79e6f4cfe6d551af0c440c093ea8edd
Francois Petit <fpetit@2G6.42angouleme.fr>
2026-01-03 21:37:15 +0100

    fix(UM): relative import

--- srcs/users/src/utils/logger.ts
+++ srcs/users/src/utils/logger.ts
@@ -1,4 +1,4 @@
 import pino from 'pino';
-import { loggerConfig } from 'src/config/logger.config.js';
+import { loggerConfig } from '../config/logger.config.js';
 
 export const logger = pino(loggerConfig);
