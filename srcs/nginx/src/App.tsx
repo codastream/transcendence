e2a90df508767546f34ebb7cf1decae1847582a2
Francois Petit <fpetit@2G6.42angouleme.fr>
2026-01-26 15:34:17 +0100

    feat(NGINX): animation on / route

--- srcs/nginx/src/App.tsx
+++ srcs/nginx/src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { ProfilePage } from './pages/ProfilePage';
 import { LoginPage } from './pages/LoginRegisterPage';
 import { useAuth } from './providers/AuthProvider';
+import { AnimationPage } from './pages/AnimationPage';
 
 const MeRedirect = () => {
   const { user } = useAuth();
@@ -14,7 +15,7 @@ export const App = () => {
   return (
     <main className="min-h-screen bd-slate-950 text-slate-100">
       <Routes>
-        <Route path="/" element={<LoginPage isRegister={false} />}></Route>
+        <Route path="/" element={<AnimationPage />}></Route>
         <Route path="/signup" element={<LoginPage isRegister={true} />}></Route>
         <Route path="/login" element={<LoginPage isRegister={false} />}></Route>
         <Route path="/me" element={<MeRedirect />}></Route>
---
+++ srcs/nginx/src/pages/AnimationPage.tsx
@@ -0,0 +1,47 @@
+import { useAuth } from '../providers/AuthProvider';
+import AnimatedLogo from '../components/atoms/AnimatedLogo';
+import Background from '../components/atoms/Background';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+interface AnimationPageProps {
+  className?: string;
+}
+
+const colors = {
+  start: '#00ff9f',
+  end: '#0088ff',
+};
+
+export const AnimationPage = ({ className = '' }: AnimationPageProps) => {
+  const { user } = useAuth();
+  const [animDone, setAnimDone] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (animDone) {
+      navigate(`/login`);
+    }
+  }, [animDone, user]);
+
+  const handleComplete = () => {
+    setAnimDone(true);
+  };
+
+  return (
+    <div className={`w-full h-full relative ${className}`}>
+      <Background
+        grainIntensity={4}
+        baseFrequency={0.28}
+        colorStart={colors.start}
+        colorEnd={colors.end}
+      >
+        <AnimatedLogo
+          className="max-w-100 lg::w-full"
+          duration={12000}
+          onComplete={handleComplete}
+        />
+      </Background>
+    </div>
+  );
+};
